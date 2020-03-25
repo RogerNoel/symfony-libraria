142869c03e803383c476f550e2917ca59687c652
RogerNoel <noel.roger.pro@gmail.com>
2020-03-25 12:03:37 +0100

    crea PanierController index() to send cart's books datas to template

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,36 +2,46 @@
 
 namespace App\Controller;
 
+use App\Repository\LivresRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class PanierController extends AbstractController
 {
     /**
      * @Route("/panier", name="panier")
      */
-    public function index()
+    public function index(SessionInterface $session, LivresRepository $livresRepository)
     {
+        // I need access to session here: HttpFoundation\Session\SessionInterface will provide it
+        // it says ( ... , I want SessionInterface and call it $session)
+        // -> this was found by typing "php bin/console debug:autowiring session" in terminal
+        $panier = $session->get('panier', []); // looks for any 'panier' data in the session, if not any -> create an empty array (like an empty cart at the store entrance!)
+        // at this point, if "panier" exists, it only contains the id of the book and the desired quantity as key=>value id=>quantity
+        // got this from ajouterAuPanier() lower
+        // We need the other book's datas to complete cart display for consumer: title, author, price ... ->
+        $livresDatas = [];
+        foreach ($panier as $id=>$quantity){
+            $livresDatas[] = [
+                "livre" => $livresRepository->find($id), // get all book datas from the book repository
+                "quantite" => $quantity // get the quantity already stored as a value in $panier
+            ];
+        }
         return $this->render('panier/index.html.twig', [
-            'controller_name' => 'PanierController',
+            'items' => $livresDatas,
         ]);
     }
 
     /**
-     * Undocumented function
-     *
      * @Route("/panier/add/{id}", name="panier_ajouteraupanier")
      */
-    public function ajouterAuPanier($id, Request $request){
-        // I need access to session here: HttpFoundation comp. provides objects to manipulate HTTP protocol (Req, Res, Session...)
-        $session = $request->getSession(); // obtain an objet whose class implements SessionInterface
-        // SessionInterface represents a "parameter bag" which e.a. adds methods to an array (add, edit ... )
-        $panier = $session->get('panier', []); // looks for any 'panier' data in the session, if not any -> create an empty array (like an empty cart at the store entrance!)
-        $panier[$id] = 1; // assigns quantity 1 to a product id in the cart -> this assumes that each second-hand book stock = 1
+    public function ajouterAuPanier($id, SessionInterface $session){
+        $panier = $session->get('panier', []);
+        $panier[$id] = 1; // assigns quantity 1 to the product whose id is in parameter in the cart 
+        // -> this assumes that each second-hand book max stock = 1
         // panier is updated now but not yet in the session, the next lines does this
         $session->set('panier', $panier); // param 1 = session cart, param 2 = updated cart
-        dd($session->get('panier'));
+        return $this->redirectToRoute('panier');
     }
-
 }
