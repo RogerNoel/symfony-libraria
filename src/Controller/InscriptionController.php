f52c6b2f62083e2f724497771f4dca882a2420e8
RogerNoel <noel.roger.pro@gmail.com>
2020-04-02 15:00:52 +0200

    fix 'labels' inClientInscriptionType

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -3,20 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Clients;
+use App\Form\ClientInscriptionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpFoundation\Request;
 
 class InscriptionController extends AbstractController
 {
     /**
      * @Route("/inscription", name="inscription")
      */
-    public function new(Request $request)
+    public function new()
     {
-        // dd($request);
         $client = new Clients();
         $client->setEmail("");
         $client->setNom("");
@@ -29,22 +26,8 @@ class InscriptionController extends AbstractController
         $client->setPays("");
         $client->setMdp("");
 
-        $form = $this->createFormBuilder($client)
-            ->add("email", TextType::class, ["label"=>"Votre E-mail"])
-            ->add("nom", TextType::class, ["label"=>"Votre nom"])
-            ->add("prenom", TextType::class, ["label"=>"Votre prénom"])
-            ->add("adresse", TextType::class, ["label"=>"Votre adresse"])
-            ->add("numero", TextType::class, ["label"=>"numéro de maison"])
-            ->add("codepostal", TextType::class, ["label"=>"Code postal"])
-            ->add("ville", TextType::class, ["label"=>"Votre ville"])
-            ->add("province", TextType::class, ["label"=>"Province"])
-            ->add("pays", TextType::class, ["label"=>"Pays"])
-            ->add("mdp", TextType::class, ["label"=>"Mot de passe"])
-            ->add("save", SubmitType::class, ["label"=>"Soumettre"])
-            ->getForm();
-
-            // https://riptutorial.com/symfony-forms/example/15519/create-a-simple-form-in-a-controller
-            
+        $form = $this->createForm(ClientInscriptionType::class, $client);
+        // dd($form);
             return $this->render("inscription/index.html.twig", [
                 'form' => $form->createView()],
             );
---
+++ src/Form/ClientInscriptionType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Clients;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClientInscriptionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('nom')
+            ->add('prenom')
+            ->add('adresse')
+            ->add('numero')
+            ->add('codepostal', TextType::class, ["label"=>"Code postal"])
+            ->add('ville')
+            ->add('province')
+            ->add('pays')
+            ->add('mdp', TextType::class, ["label"=>"Mot de passe"])
+            ->add('Soumettre', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Clients::class,
+        ]);
+    }
+}
